ff7e281efb1366b50e53cc07be9b7df468abb5f7
yurguenP <yurguen0071@gmail.com>
2020-10-27 07:47:24 -0400

    Creacion rama develop

---
+++ BackEnd/src/models/Photo.ts
@@ -0,0 +1,17 @@
+
+import { Schema, model, DocumentToObjectOptions} from 'mongoose';
+
+
+const schema = new Schema({
+    title: String,
+    description: String,
+    imagePath: String
+});
+
+interface IPhoto extends Document {
+    title: string;
+    description: string;
+    imagePath: string;
+}
+
+export default model<IPhoto>('Photo', schema);
