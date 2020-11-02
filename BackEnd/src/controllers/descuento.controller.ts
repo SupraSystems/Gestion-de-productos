172e2bd7d1dd212df834c10d4ac6480404bffc6a
Kevin Calani <Kevincalani2311@gmail.com>
2020-11-02 12:45:52 -0600

    agregando funcionalidades a descuento

--- BackEnd/src/controllers/descuento.controller.ts
+++ BackEnd/src/controllers/descuento.controller.ts
@@ -9,7 +9,13 @@ export async function getDescuentos(req: Request, res: Response): Promise<Respon
 
 export async function createDescuento(req: Request, res: Response): Promise<Response> {
     const { _id, fechaini, fechafin,porcentaje, cantidad } = req.body;
-    const nuevoDescuento = { _id, fechaini, fechafin,porcentaje, cantidad };
+    const nuevoDescuento = { 
+        _id,
+        fechaini,
+        fechafin,
+        porcentaje, 
+        cantidad 
+    };
     const descuento = new Descuento(nuevoDescuento);
     await descuento.save();
     return res.json({
@@ -34,10 +40,41 @@ export async function updateDescuento(req: Request, res: Response): Promise<Resp
     const { _id } = req.params;
     const { fechaini, fechafin, porcentaje, cantidad } = req.body;
     const DescuentoActualizado = await Descuento.findByIdAndUpdate(_id, {
-        _id, fechaini, fechafin, porcentaje, cantidad
+        _id, 
+        fechaini, 
+        fechafin, 
+        porcentaje, 
+        cantidad
     });
     return res.json({
         message: 'Descuento Actualizado',
         DescuentoActualizado
     });
+}
+
+export async function getDescuentoCombo(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const des = await Descuento.findById(id);
+    const descuento = des!.coddescuento
+    const desc = await Descuento.findById(descuento)
+    return res.json(desc);
+}
+
+export async function getDesc(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const detalle = await Descuento.find({idProducto:id});
+    return res.json(detalle);
+}
+
+export async function getFoto(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const detalle = await Descuento.findById(id);
+    const foto= detalle!.imagePath;
+    return res.send('<img src=http://localhost:4000/'+foto+'>');
+}
+
+export async function getCategoria(req: Request, res: Response): Promise<Response> {
+    const { tipo } = req.params;
+    const combos = await Descuento.find({tipo:tipo});
+    return res.json(combos);
 }
